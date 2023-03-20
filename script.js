648218fd2875998b1893fba3a77eb16be938fe07
shobhit0111 <shobhitbhatnagar61@gmail.com>
2023-03-20 21:20:24 +0530

    Added

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
   
     // typing text animation script
     var typed = new Typed(".typing", {
-      strings: ["Back-End Developer", "Daydreamer"],
+      strings: ["Full Stack Web Developer", "Daydreamer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
     });
   
     var typed = new Typed(".typing-2", {
-      strings: ["Back-End Developer", "Daydreamer"],
+      strings: ["Full Stack Web Developer", "Daydreamer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
