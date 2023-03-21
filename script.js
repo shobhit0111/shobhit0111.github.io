a8195089f14edb9ae9158733cf41e104e1fc037c
shobhit0111 <shobhitbhatnagar61@gmail.com>
2023-03-21 13:08:37 +0530

    Changes done

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
       $("html").css("scrollBehavior", "auto");
     });
   
-    $(".navbar .menu li a").click(function () {
+    $("#nav-menu .menu li a").click(function () {
       // applying again smooth scroll on menu items click
       $("html").css("scrollBehavior", "smooth");
     });
