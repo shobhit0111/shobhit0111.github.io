2d55d816a3179d7af2d4843149cdd6f199a8122d
shobhit0111 <shobhitbhatnagar61@gmail.com>
2023-03-21 11:34:58 +0530

    Chnages done

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
   
     // typing text animation script
     var typed = new Typed(".typing", {
-      strings: ["Full Stack Web Developer", "Daydreamer"],
+      strings: ["Full Stack Web Developer", "Fastlearner"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
     });
   
     var typed = new Typed(".typing-2", {
-      strings: ["Full Stack Web Developer", "Daydreamer"],
+      strings: ["Full Stack Web Developer", "Fastlearner"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
